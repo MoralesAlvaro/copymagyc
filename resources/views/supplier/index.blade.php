f54f970ea57a01dd079f1d6e613ee50825d047ff
Edwin <edgoca2498@gmail.com>
2021-10-03 21:56:57 -0600

    :tada: Initial commit

--- resources/views/supplier/index.blade.php
+++ resources/views/supplier/index.blade.php
@@ -3,7 +3,9 @@
 
 <div class="container">
   <div>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat vitae eveniet quos obcaecati ab ipsam. Dolorem, cum rem eius odit, quod aliquid laborum pariatur culpa voluptas ut soluta reprehenderit maxime?
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat vitae eveniet quos obcaecati ab ipsam. 
+    Dolorem, cum rem eius odit, quod aliquid laborum pariatur culpa voluptas ut soluta reprehenderit maxime?
+    Lorem
   </div>
 </div>
 
