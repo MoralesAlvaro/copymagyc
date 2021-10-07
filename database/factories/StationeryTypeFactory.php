1dd6199df50368dcc2b380c2140ee8c853c28a15
Alvaro Flores <moralesalvaro308@gmail.com>
2021-10-06 21:19:16 -0600

    Agregando modelo, seeder, factory StationeryTypes

---
+++ database/factories/StationeryTypeFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\StationeryType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class StationeryTypeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = StationeryType::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
