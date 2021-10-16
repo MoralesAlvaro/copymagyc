7b022ae13d0b38a292a17fbc7bb67d5effdfe8ed
Alvaro Flores <moralesalvaro308@gmail.com>
2021-10-15 23:22:15 -0600

    Agregando mensaje warning suppliers

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -144,13 +144,13 @@ class SupplierController extends Controller
         $data = Supplier::find($id);
         try {
             if ($data == null) {
-                return redirect('/supplier')->with('success', 'El registro que desea eliminar no se encuentra!.');
+                return redirect('/supplier')->with('warning', 'El registro que desea eliminar no se encuentra!.');
             }
             
             $data->delete();
             return redirect('/supplier')->with('success', 'El registro se ha sido eliminado correctamente!.');
         } catch (\Throwable $th) {
-            return redirect('/supplier')->with('success', 'No puede eliminar un proveedor relacionado con una materia prima!.');
+            return redirect('/supplier')->with('warning', 'No puede eliminar un proveedor relacionado con una materia prima!.');
         }
     }
 }
